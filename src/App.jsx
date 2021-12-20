1dadcd380f65770772db506fe5586dbabd00b94a
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-12-20 19:53:05 +0900

    react project init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {CssBaseline, Grid} from '@material-ui/core';
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
+import PlaceDetails from "./Components/PlaceDetails/PlaceDetails";
+
+const App = () => {
+  return(
+    <>
+      
+    </>
+  )
+}
+
+export default App;
