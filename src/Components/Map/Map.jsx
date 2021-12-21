91a861d822e2fb879dfe82be332fe5ae9d297813
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-12-21 21:13:35 +0900

    Map.jsx update, styling

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery} from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
+
+import useStyles from './styles'
+
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+
+  const coordinates = { lat: 37.57, lng: 126.99 };
+
   return (
-    <h1>
-      Map
-    </h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyByxI8mL-Idccft3wvtAvPKjrahZWdeUaM" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      
+      ></GoogleMapReact>
+    </div>
   );
 };
 
